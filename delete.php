85f1f4f0517ca33b6f0bdfc592df3946553b32dc
Lilian Kempees <0926313@hr.nl>
2019-01-22 17:52:08 +0100

    Added a way to delete a certain appointment from overview.

---
+++ delete.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once "db_connection.php";
+
+if (!empty($_GET["id"])) {
+    $id = mysqli_escape_string($conn, $_GET["id"]);
+    $sql = ("DELETE FROM appointments WHERE id ='$id'");
+    mysqli_query($conn, $sql);
+    mysqli_close($conn);
+    header ("Location: overview_appointments.php");
+}
+?>
+
